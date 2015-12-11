82e2040e67a3d0fe3c54a1a201b867d9b0d1944a
Icely <icely.scratch@gmail.com>
2015-12-10 20:47:32 -0500

    Created file

---
+++ userscript.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        Scratch Message Count
+// @namespace   https://github.com/Icelys/
+// @include     *
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var user = "Put your username here"
+
+var messageCount = 0;
+var startTitle = document.title;
+console.log(startTitle);
+var debug = false;
+
+
+function update(data){
+  if(debug){
+	console.log("Retrieved: " + data);
+	}
+	var decodedData = JSON.parse(data);
+	messageCount = decodedData.msg_count;
+	if(decodedData.err == undefined){
+		document.title = startTitle+" ("+messageCount+")";
+	} else {
+		document.title = startTitle;
+	}
+}
+
+function getData(){
+  if(debug){
+	console.log("Getting data...");
+	}
+	var XMLRequest = new XMLHttpRequest();
+	XMLRequest.onreadystatechange = function() {
+		if (XMLRequest.readyState == 4) {
+        		update(XMLRequest.responseText);
+        		
+    		}
+	}
+	XMLRequest.open("GET", "https://api.scratch.mit.edu/proxy/users/"+user+"/activity/count", true);
+	XMLRequest.send(null);
+}
+
+setInterval(getData, 4000);
